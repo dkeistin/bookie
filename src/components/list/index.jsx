61af471a45c6704d324eb6873cbcd4fdf7d589fa
dmytro <ssadowy85@gmail.com>
2021-03-16 16:25:39 +0200

    Create list component

---
+++ src/components/list/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// Styles
+import './styles.sass';
+
+const List = ({ header, items, handleSelect, selected }) => {
+
+  const isActive = currentId => selected.some(id => id === currentId);
+
+  return (
+    <div className="list">
+      <div className="list__header">
+        {header}
+      </div>
+      <div className="list__items">
+        {items.map(({ id, name, price }) => (
+          <div key={id} className={`list__item ${isActive(id) ? 'is-active' : ''}`} onClick={() => { handleSelect(id) }}>
+            <div className="list__item-name">{name}</div>
+            <div className="list__item-price">{price}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default List;
