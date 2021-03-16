8403bf767c1da6ea3d1aeeab15445b136b0109da
dmytro <ssadowy85@gmail.com>
2021-03-16 11:51:23 +0200

    Set height: 100vh to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from 'modules/header';
 // Pages
 import HomePage from 'pages/home-page';
+// Styles
+import './app.sass';
 
 const App = () => {
   return (
