8c54176a707ac5d071baa9fc6fa82a9abc61a8eb
dmytro <ssadowy85@gmail.com>
2021-03-18 19:40:32 +0200

    Connect open-bets module to open-bets redux

--- src/modules/open-bets/index.jsx
+++ src/modules/open-bets/index.jsx
@@ -1,37 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+// Redux
+import { fetchOpenBetsRequest } from 'redux/open-bets/actions';
+import { selectOpenBets } from 'redux/open-bets/selectors';
 // UI
 import OpenBet from 'modules/open-bet';
 import Typography from 'components/typography';
 import Scrollbar from 'components/scrollbar';
+import Spinner from 'components/spinner';
+import ErrorIndicator from 'components/error-indicator';
 // Styles
 import './styles.sass';
 
-const OpenBets = ({ bets }) => {
+const OpenBets = ({ fetchOpenBetsRequest, openBets: { loading, data, error } }) => {
+  React.useLayoutEffect(() => {
+    fetchOpenBetsRequest()
+  }, [fetchOpenBetsRequest]);
+
   return (
     <div className="open-bets">
       <div className="open-bets__container">
-        {bets.length === 0 ?
+        {error && <ErrorIndicator retry={fetchOpenBetsRequest} />}
+        {loading && <Spinner boxed />}
+
+        {data && data.length === 0 &&
           <div className="open-bets__empty">
             <Typography component="h3" className="open-bets__empty-title">Open Bets is Empty</Typography>
             <p className="open-bets__empty-message">
               Learn how to place bets <a href="http://example.com" className="open-bets__empty-link">here</a>
             </p>
           </div>
-          :
+        }
+
+        {data && data.length > 0 &&
           <div className="open-bets__content">
             <div className="open-bets__header">
               <p className="open-bets__header-item">
-                <span className="open-bets__header-item-accent">Date</span><br />
-                Time
-              </p>
+                <span className="open-bets__header-item-accent">Date</span><br />Time</p>
               <p className="open-bets__header-item">
-                <span className="open-bets__header-item-accent">Bet Amount</span><br />
-                Remaining Balance
+                <span className="open-bets__header-item-accent">Bet Amount</span><br />Remaining Balance
               </p>
             </div>
             <div className="open-bets__items">
               <Scrollbar className="open-bets__items-scroll">
-                {bets.map(({ id, date, time, betAmount, remainingBalance, placed, placedPrice, game, gamePrice, title }) => (
+                {data.map(({ id, date, time, betAmount, remainingBalance, placed, placedPrice, game, gamePrice, title }) => (
                   <div key={id} className="open-bets__item">
                     <OpenBet
                       date={date}
@@ -55,4 +68,12 @@ const OpenBets = ({ bets }) => {
   );
 };
 
-export default OpenBets;
+const mapStateToProps = createStructuredSelector({
+  openBets: selectOpenBets
+});
+
+const mapDispatchToProps = {
+  fetchOpenBetsRequest
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OpenBets);
