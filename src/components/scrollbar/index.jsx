8680c6b976aa4c00aa5f97f800c6927a3c7abc99
dmytro <ssadowy85@gmail.com>
2021-03-18 17:11:54 +0200

    Refactor scrollbar component

--- src/components/scrollbar/index.jsx
+++ src/components/scrollbar/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import SimpleBar from 'simplebar-react'
+import SimpleBar from 'simplebar-react';
+import classNames from 'classnames';
 // Styles
 import './styles.sass';
 
-const Scrollbar = ({ children, maxHeight }) => {
+const Scrollbar = ({ children, className, maxHeight }) => {
+  const classes = classNames({
+    'custom-scroll': true,
+    [className]: className
+  });
+
   return (
-    <SimpleBar style={{ maxHeight }}>
-      { children}
+    <SimpleBar className={classes} style={{ maxHeight }}>
+      {children}
     </SimpleBar>
   );
 };
