1022f2e38b536f6d729cd999ea67ca9c8b7f5873
dmytro <ssadowy85@gmail.com>
2021-03-19 18:11:08 +0200

    Create help module

--- src/pages/help-page/index.jsx
+++ src/pages/help-page/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // UI
 import PageLayout from 'components/page-layout';
+import Help from 'modules/help';
 
 const HelpPage = () => {
   return (
     <PageLayout>
+      <Help />
     </PageLayout>
   );
 };
