0b828e596392622db2346e4f84914d2d0c73512c
dmytro <ssadowy85@gmail.com>
2021-03-19 13:56:24 +0200

    Remove unused styles from transactions component

--- src/pages/transactions-page/index.jsx
+++ src/pages/transactions-page/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 // UI
 import PageLayout from 'components/page-layout';
 import Transactions from 'modules/transactions';
-// Styles
-import './styles.sass';
 
 const TransactionsPage = () => {
   return (
-    <PageLayout className="transactions-page">
+    <PageLayout>
       <Transactions />
     </PageLayout>
   );
