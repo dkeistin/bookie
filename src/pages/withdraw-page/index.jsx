5401fd6bf2f95f1b6f4b749a0f55aa20d049adbb
dmytro <ssadowy85@gmail.com>
2021-03-20 13:04:57 +0200

    Implement routing between withdraw and withdraw-details

--- src/pages/withdraw-page/index.jsx
+++ src/pages/withdraw-page/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 // UI
 import PageLayout from 'components/page-layout';
 import WithdrawDetails from 'modules/withdraw-details';
+import Withdraw from 'modules/withdraw';
 
 const WithdrawPage = () => {
   return (
     <PageLayout>
-      <WithdrawDetails />
+      <Switch>
+        <Route path="/withdraw" exact component={Withdraw} />
+        <Route path="/withdraw/details" exact component={WithdrawDetails} />
+        <Redirect to="/withdraw" />
+      </Switch>
     </PageLayout>
   );
 };
