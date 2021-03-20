944c97b2286d09bf55efc262122ae7ab74c6704a
dmytro <ssadowy85@gmail.com>
2021-03-20 13:54:35 +0200

    Create deposit module; Add deposit module to deposit page

---
+++ src/modules/deposit/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// UI
+import PaymentOperation from 'modules/payment-operation';
+
+const Deposit = () => {
+  return (
+    <PaymentOperation type="deposit" />
+  );
+};
+
+export default Deposit;
--- src/pages/deposit-page/index.jsx
+++ src/pages/deposit-page/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 // UI
 import PageLayout from 'components/page-layout';
+import Deposit from 'modules/deposit';
 
 const DepositPage = () => {
   return (
     <PageLayout>
       <Switch>
-        <Route path="/deposit" exact component={<></>} />
+        <Route path="/deposit" exact component={Deposit} />
         <Route path="/deposit/details" exact component={<></>} />
         <Redirect to="/deposit" />
       </Switch>
