293bb05f34b30c8c4a07995a06ae7474b70c17ce
dmytro <ssadowy85@gmail.com>
2021-03-20 10:30:29 +0200

    Create fee-calculator component

---
+++ src/components/fee-calculator/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import classNames from 'classnames';
+// UI
+import Typography from 'components/typography';
+import Input from 'components/input';
+// Styles
+import './styles.sass';
+// Assets
+import { ReactComponent as LitecoinIcon } from 'assets/images/coins/litecoin.svg';
+import { ReactComponent as BitcoinIcon } from 'assets/images/coins/bitcoin.svg';
+
+const FeeCalculator = ({ className }) => {
+  const classes = classNames({
+    'fee-calculator': true,
+    [className]: className
+  });
+
+  return (
+    <div className={classes}>
+      <Typography className="fee-calculator__title" component="h4">Transaction Fee Calculator</Typography>
+      <div className="fee-calculator__box">
+        <div className="fee-calculator__item">
+          <Typography component="h5" className="fee-calculator__item-title">Withdraw Amount</Typography>
+          <Input placeholder="$50.00" />
+        </div>
+        <div className="fee-calculator__item">
+          <Typography component="h5" className="fee-calculator__item-title">Estimated Fees</Typography>
+          <div className="fee-calculator__coins">
+            <div className="fee-calculator__coin">
+              <LitecoinIcon className="fee-calculator__coin-icon" />
+              <span className="fee-calculator__coin-price">$1.75</span>
+            </div>
+            <div className="fee-calculator__coin">
+              <BitcoinIcon className="fee-calculator__coin-icon" />
+              <span className="fee-calculator__coin-price">$0.09</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeeCalculator;
