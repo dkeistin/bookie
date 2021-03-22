aae42bd3c2f5b5514f2a668a272d33b1eeadac18
dmytro <ssadowy85@gmail.com>
2021-03-22 11:19:15 +0200

    Add apex-chart to the project; Add basic view for referrals charts

--- src/screens/referrals/data-preview-content.js
+++ src/screens/referrals/data-preview-content.js
@@ -1,8 +1,38 @@
 import React from 'react';
 // UI
 import Typography from 'components/typography';
+import Chart from "react-apexcharts";
 
 export const dataPreviewContent = () => {
+  const chart = {
+    options: {
+      chart: {
+        type: 'area'
+      },
+      colors: ['#AD6CFF'],
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth'
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+      },
+      tooltip: {
+        x: {
+          format: 'dd/MM/yy HH:mm'
+        },
+      },
+    },
+    series: [
+      {
+        name: 'series1',
+        data: [31, 40, 28, 51, 42, 109, 100]
+      }
+    ]
+  };
   return [
     {
       header: () => (
@@ -10,6 +40,11 @@ export const dataPreviewContent = () => {
       ),
       body: () => (
         <div className="rewards-screen__info">
+          <Chart
+            options={chart.options}
+            series={chart.series}
+            type="area"
+          />
         </div>
       ),
     },
@@ -19,6 +54,11 @@ export const dataPreviewContent = () => {
       ),
       body: () => (
         <div className="rewards-screen__info">
+          <Chart
+            options={chart.options}
+            series={chart.series}
+            type="area"
+          />
         </div>
       ),
     },
