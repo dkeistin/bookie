56bd1b3f02d2eac09f713462c2b4809affc61c91
dmytro <ssadowy85@gmail.com>
2021-03-22 11:58:15 +0200

    Update homescreen responsive styles

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -10,10 +10,12 @@ const HomeScreen = () => {
   return (
     <div className="home-screen">
       <Container>
-        <div className="home-screen__info">
-          <Typography component="h1" className="home-screen__title">Join today and have your favorite players carry you to victory</Typography>
-          <Typography component="p" className="p-xl home-screen__text">Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</Typography>
-          <Button variant="accent" size="xl">Join now</Button>
+        <div className="home-screen__row">
+          <div className="home-screen__info">
+            <Typography component="h1" className="home-screen__title">Join today and have your favorite players carry you to victory</Typography>
+            <Typography component="p" className="p-xl home-screen__text">Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</Typography>
+            <Button variant="accent" size="xl">Join now</Button>
+          </div>
         </div>
       </Container>
     </div>
