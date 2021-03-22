7e14c3f2398730fa0168ae120272d4e8ca8fc31e
dmytro <ssadowy85@gmail.com>
2021-03-22 11:42:42 +0200

    Refactor fee-calculator

--- src/components/fee-calculator/index.jsx
+++ src/components/fee-calculator/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 // UI
+import Box from 'components/box';
 import Typography from 'components/typography';
 import Input from 'components/input';
 // Styles
@@ -15,30 +16,32 @@ const FeeCalculator = ({ className, type }) => {
     [className]: className
   });
 
-  const title = type === 'deposit' ? 'Deposit': 'Withdraw';
+  const title = type === 'deposit' ? 'Deposit' : 'Withdraw';
 
   return (
     <div className={classes}>
       <Typography className="fee-calculator__title" component="h4">Transaction Fee Calculator</Typography>
-      <div className="fee-calculator__box">
-        <div className="fee-calculator__item">
-          <Typography component="h5" className="fee-calculator__item-title">{title} Amount</Typography>
-          <Input placeholder="$50.00" />
-        </div>
-        <div className="fee-calculator__item">
-          <Typography component="h5" className="fee-calculator__item-title">Estimated Fees</Typography>
-          <div className="fee-calculator__coins">
-            <div className="fee-calculator__coin">
-              <LitecoinIcon className="fee-calculator__coin-icon" />
-              <span className="fee-calculator__coin-price">$1.75</span>
-            </div>
-            <div className="fee-calculator__coin">
-              <BitcoinIcon className="fee-calculator__coin-icon" />
-              <span className="fee-calculator__coin-price">$0.09</span>
+      <Box className="fee-calculator__box">
+        <div className="fee-calculator__items">
+          <div className="fee-calculator__item">
+            <Typography component="h5" className="fee-calculator__item-title">{title} Amount</Typography>
+            <Input placeholder="$50.00" />
+          </div>
+          <div className="fee-calculator__item">
+            <Typography component="h5" className="fee-calculator__item-title">Estimated Fees</Typography>
+            <div className="fee-calculator__coins">
+              <div className="fee-calculator__coin">
+                <LitecoinIcon className="fee-calculator__coin-icon" />
+                <span className="fee-calculator__coin-price">$1.75</span>
+              </div>
+              <div className="fee-calculator__coin">
+                <BitcoinIcon className="fee-calculator__coin-icon" />
+                <span className="fee-calculator__coin-price">$0.09</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Box>
     </div>
   );
 };
