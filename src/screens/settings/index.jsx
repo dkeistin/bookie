c6cba2e1c5a2c46e20fdd3d13e6151a792c69c0d
dmytro <ssadowy85@gmail.com>
2021-03-23 18:42:13 +0200

    Create settings screen

--- src/screens/settings/index.jsx
+++ src/screens/settings/index.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 import ScreenLayout from 'components/screen-layout';
 import ContentBox from 'components/content-box';
 import Typography from 'components/typography';
+import AvatarEditor from 'components/avatar-editor';
+import Input from 'components/input';
+import Select from 'components/select';
+import Button from 'components/button';
 // Styles
 import './styles.sass';
+// Assets
+import AvatarImage from 'assets/images/user/user-fullsize.jpg';
 
 const SettingsScreen = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <ScreenLayout>
       <ContentBox className="settings-screen">
@@ -14,6 +24,44 @@ const SettingsScreen = () => {
           <Typography component="h3">Settings</Typography>
         </ContentBox.Header>
         <ContentBox.Body>
+          <form className="settings-screen__body" onSubmit={handleSubmit}>
+            <div className="settings-screen__item">
+              <Typography component="h4" className="settings-screen__item-title">Profile picture</Typography>
+              <div className="settings-screen__item-box">
+                <AvatarEditor image={AvatarImage} />
+              </div>
+            </div>
+            <div className="settings-screen__item">
+              <Typography component="h4" className="settings-screen__item-title">Username</Typography>
+              <div className="settings-screen__item-box">
+                <Input value="Fortbettor" onChange={() => { }} />
+              </div>
+            </div>
+            <div className="settings-screen__item">
+              <Typography component="h4" className="settings-screen__item-title">Email</Typography>
+              <div className="settings-screen__item-box">
+                <Input value="Fortbettor@gmail.com" onChange={() => { }} />
+              </div>
+            </div>
+            <div className="settings-screen__item">
+              <Typography component="h4" className="settings-screen__item-title">Password</Typography>
+              <div className="settings-screen__item-box">
+                <Input value="******123" onChange={() => { }} />
+              </div>
+            </div>
+            <div className="settings-screen__item">
+              <Typography component="h4" className="settings-screen__item-title">Time Zone</Typography>
+              <div className="settings-screen__item-box">
+                <Select
+                  defaultValue={{ label: 'Pacific standard', value: 'pacific standard' }}
+                  options={[
+                    { label: 'Pacific standard', value: 'pacific standard' },
+                    { label: 'Pacific standard', value: 'pacific standard' },
+                  ]} />
+              </div>
+            </div>
+            <Button variant="primary" size="xl" type="submit">Save</Button>
+          </form>
         </ContentBox.Body>
       </ContentBox>
     </ScreenLayout>
