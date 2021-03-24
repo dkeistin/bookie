c5024eef8efa85d2b36f62a90cadb5ac717a0652
dmytro <ssadowy85@gmail.com>
2021-03-24 11:11:29 +0200

    Update regions reducer initial state

--- src/redux/selected-region/reducer.js
+++ src/redux/selected-region/reducer.js
@@ -1,6 +1,6 @@
 import { SelectedRegionTypes } from './types';
 
-const INITIAL_STATE = 'nae';
+const INITIAL_STATE = 'all';
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
