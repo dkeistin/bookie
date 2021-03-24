aa17f1addbdd13a4f89c846e7858aef90977f4e9
dmytro <ssadowy85@gmail.com>
2021-03-24 11:00:19 +0200

    Refactor select component

--- src/components/select/index.jsx
+++ src/components/select/index.jsx
@@ -4,12 +4,13 @@ import ReactSelect from 'react-select';
 // Styles
 import { colourStyles } from './styles';
 
-const Select = ({ options, defaultValue }) => {
+const Select = ({ options, defaultValue, ...otherProps }) => {
   return (
     <ReactSelect
       styles={colourStyles}
       options={options}
       defaultValue={defaultValue}
+      {...otherProps}
     />
   );
 };
