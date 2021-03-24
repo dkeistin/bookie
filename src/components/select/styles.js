a2684c6d042caedc7c6d5a7aaa5ee8ee9260d053
dmytro <ssadowy85@gmail.com>
2021-03-24 10:21:34 +0200

    Fix select z-index bug

--- src/components/select/styles.js
+++ src/components/select/styles.js
@@ -15,11 +15,12 @@ export const colourStyles = {
     fontSize: '14px'
   }),
   indicatorSeparator: () => ({ display: 'none' }),
-  option: (styles, { data, isDisabled, isFocused, isSelected }) => ({
+  option: (styles, { isDisabled, isFocused, isSelected }) => ({
     ...styles,
     color: isSelected ? 'white' : '#3C4146',
     fontWeight: '500',
     fontSize: '14px',
     backgroundColor: isDisabled ? null : isSelected ? '#5E00D6' : isFocused ? '#f3f4f7' : null,
-  })
+  }),
+  menu: styles => ({ ...styles, zIndex: 9999 })
 };
