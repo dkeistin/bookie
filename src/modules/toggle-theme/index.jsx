287e12c346474d611df7465e673923bb2d8dd887
dmytro <ssadowy85@gmail.com>
2021-03-25 10:54:05 +0200

    Add theme-context to toggle-theme component

--- src/modules/toggle-theme/index.jsx
+++ src/modules/toggle-theme/index.jsx
@@ -4,14 +4,15 @@ import ToggleSwitch from 'components/toggle-switch';
 import Typography from 'components/typography';
 // Styles
 import './styles.sass';
+import { useTheme } from 'context/theme-context';
 
 const ToggleTheme = () => {
-  const [isActive, setIsActive] = React.useState(false)
-  const handleToggle = () => setIsActive(isActive => !isActive)
+  const { dark, toggleTheme } = useTheme();
+
   return (
     <div className="toggle-theme">
-      <ToggleSwitch onChange={handleToggle} checked={isActive} className="toggle-theme__switch" />
-      <Typography component="span" variant="h5" className="user-dropdown-item__title">{isActive ? 'Dark theme' : 'Light theme'}</Typography>
+      <ToggleSwitch onChange={toggleTheme} checked={dark} className="toggle-theme__switch" />
+      <Typography component="span" variant="h5" className="user-dropdown-item__title">{dark ? 'Dark theme' : 'Light theme'}</Typography>
     </div>
   );
 };
