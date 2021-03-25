f0edb88d49624b9fce92a4234f3794431021f487
dmytro <ssadowy85@gmail.com>
2021-03-25 10:53:42 +0200

    Fix error in toggle-switch component

--- src/components/toggle-switch/index.jsx
+++ src/components/toggle-switch/index.jsx
@@ -12,7 +12,7 @@ const ToggleSwitch = ({ onChange, checked, className }) => {
   return (
     <div className={classes}>
       <input type="checkbox" className="switch__input" id="switch" onChange={onChange} checked={checked}/>
-      <label for="switch" className="switch__label"></label>
+      <label htmlFor="switch" className="switch__label"></label>
     </div>
   );
 };
