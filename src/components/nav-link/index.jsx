5a4ab4b5f400b6839380d3d648551c0bf33b11b2
dmytro <ssadowy85@gmail.com>
2021-03-25 11:45:55 +0200

    Adapt nav-link component to dark theme

--- src/components/nav-link/index.jsx
+++ src/components/nav-link/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import classNames from 'classnames';
 import Typography from 'components/typography';
+// Context
+import { useTheme } from 'context/theme-context';
 // Styles
 import './styles.sass';
 
 const NavLink = ({ children, className, isActive, accent, ...otherProps }) => {
+  const { dark } = useTheme();
+
   const classes = classNames({
     'nav-link': true,
     'is-active': isActive,
     'nav-link--accent': accent,
+    'nav-link--dark': dark,
     [className]: className
   });
 
