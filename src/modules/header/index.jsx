95681e99bbcbbb27950592121b10f2b3b31f9d6f
dmytro <ssadowy85@gmail.com>
2021-03-25 14:42:41 +0200

    Implement header mobile behaviour

--- src/modules/header/index.jsx
+++ src/modules/header/index.jsx
@@ -12,6 +12,7 @@ import Button from 'components/button';
 import Regions from 'modules/regions';
 import UserDropdown from 'modules/user-dropdown';
 import Burger from 'components/burger';
+import Overlay from 'components/overlay';
 // Assets
 import { ReactComponent as Logo } from 'assets/images/logo.svg';
 // Utils
@@ -24,6 +25,8 @@ const Header = ({ user: { userData }, breakpoints, currentBreakpoint }) => {
   const location = useLocation();
 
   const [isMobile, setIsMobile] = React.useState(false);
+  const [showMenu, setShowMenu] = React.useState(false);
+
   React.useEffect(() => {
     if (breakpoints[currentBreakpoint] < breakpoints.xl) {
       setIsMobile(true);
@@ -31,6 +34,8 @@ const Header = ({ user: { userData }, breakpoints, currentBreakpoint }) => {
       setIsMobile(false);
     }
   }, [breakpoints, currentBreakpoint]);
+
+  const toggleMenu = () => setShowMenu(showMenu => !showMenu);
 
   return (
     <Fragment>
@@ -42,56 +47,85 @@ const Header = ({ user: { userData }, breakpoints, currentBreakpoint }) => {
               <Logo className="header__logo" />
             </Link>
           </div>
-          {userData &&
+          {(userData && !isMobile) &&
             <div className="header__center">
               <Regions />
             </div>
           }
           <div className="header__right">
-            {userData ?
-              <div className="header__widgets">
-                {!isMobile &&
-                  <Fragment>
-                    <div className="header__widget">
-                      <NavLink isActive={isMathcingPath(location, '/withdraw')} onClick={() => history.push('/withdraw')}>Withdraw</NavLink>
-                    </div>
-                    <div className="header__widget">
-                      <Button size="lg" variant="accent" onClick={() => history.push('/deposit')}>Deposit</Button>
-                    </div>
-                  </Fragment>
-                }
+            <div className="header__widgets">
+              {(!userData && !isMobile) &&
+                <Fragment>
+                  <div className="header__widget">
+                    <Button size="lg" variant="accent" onClick={() => history.push('/sign-up')}>Sign Up</Button>
+                  </div>
+                  <div className="header__widget">
+                    <Button size="lg" variant="primary" onClick={() => history.push('/sign-in')}>Sign In</Button>
+                  </div>
+                </Fragment>
+              }
+              {(userData && !isMobile) &&
+                <Fragment>
+                  <div className="header__widget">
+                    <NavLink isActive={isMathcingPath(location, '/withdraw')} onClick={() => history.push('/withdraw')}>Withdraw</NavLink>
+                  </div>
+                  <div className="header__widget">
+                    <Button size="lg" variant="accent" onClick={() => history.push('/deposit')}>Deposit</Button>
+                  </div>
+                </Fragment>
+              }
+              {userData &&
                 <div className="header__widget">
                   <UserDropdown userData={userData} />
                 </div>
-                {isMobile &&
-                  <div className="header__widget header__burger">
-                    <Burger />
-                  </div>
-                }
-              </div>
-              :
-              <div className="header__widgets">
-                {!isMobile &&
-                  <Fragment>
-                    <div className="header__widget">
-                      <Button size="lg" variant="accent" onClick={() => history.push('/sign-up')}>Sign Up</Button>
-                    </div>
-                    <div className="header__widget">
-                      <Button size="lg" variant="primary" onClick={() => history.push('/sign-in')}>Sign In</Button>
-                    </div>
-                  </Fragment>
-                }
-                {isMobile &&
-                  <div className="header__widge header__burger">
-                    <Burger />
-                  </div>
-                }
-              </div>
-            }
+              }
+              {isMobile &&
+                <div className="header__widge header__burger">
+                  <Burger onClick={toggleMenu} />
+                </div>
+              }
+            </div>
           </div>
         </Container>
+
+        {isMobile &&
+          <Fragment>
+            <Overlay isActive={showMenu} zIndex="999" onClick={toggleMenu} fixed />
+            <div className={`header__mobile ${showMenu ? 'is-active' : ''}`}>
+              {!userData &&
+                <div className="header__mobile-line">
+                  <div className="header__widgets">
+                    <div className="header__widget" onClick={toggleMenu} >
+                      <Button size="lg" variant="accent" onClick={() => history.push('/sign-up')}>Sign Up</Button>
+                    </div>
+                    <div className="header__widget" onClick={toggleMenu} >
+                      <Button size="lg" variant="primary" onClick={() => history.push('/sign-in')}>Sign In</Button>
+                    </div>
+                  </div>
+                </div>
+              }
+              {userData &&
+                <Fragment>
+                  <div className="header__mobile-line">
+                    <Regions onItemClick={toggleMenu} />
+                  </div>
+                  <div className="header__mobile-line">
+                    <div className="header__widgets">
+                      <div className="header__widget" onClick={toggleMenu} >
+                        <NavLink isActive={isMathcingPath(location, '/withdraw')} onClick={() => history.push('/withdraw')}>Withdraw</NavLink>
+                      </div>
+                      <div className="header__widget" onClick={toggleMenu} >
+                        <Button size="lg" variant="accent" onClick={() => history.push('/deposit')}>Deposit</Button>
+                      </div>
+                    </div>
+                  </div>
+                </Fragment>
+              }
+            </div>
+          </Fragment>
+        }
       </header>
-    </Fragment>
+    </Fragment >
   );
 };
 
