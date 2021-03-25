143bebaeac40f4f88bebd2c3340eb872914f49e4
dmytro <ssadowy85@gmail.com>
2021-03-25 16:20:14 +0200

    Add active state to user-dropdown-item module

--- src/modules/user-dropdown-item/index.jsx
+++ src/modules/user-dropdown-item/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import classNames from 'classnames';
 // UI
 import Typography from 'components/typography';
 // Styles
 import './styles.sass';
 
-const UserDropdownItem = ({ title, icon: Icon, onClick }) => {
+const UserDropdownItem = ({ title, icon: Icon, onClick, className, isActive }) => {
+  const classes = classNames({
+    'user-dropdown-item': true,
+    'is-active': isActive,
+    [className]: className
+  });
   return (
-    <div className="user-dropdown-item" onClick={onClick}>
+    <div className={classes} onClick={onClick}>
       <Icon className="user-dropdown-item__icon" />
       <Typography component="span" variant="h5" className="user-dropdown-item__title">{title}</Typography>
     </div>
