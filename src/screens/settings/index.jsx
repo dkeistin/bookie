10af865ee101b6e93dfdd1fdb027243d0b90464b
dmytro <ssadowy85@gmail.com>
2021-03-25 13:54:51 +0200

    Implement validation of settings screen form

--- src/screens/settings/index.jsx
+++ src/screens/settings/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 // UI
 import ScreenLayout from 'components/screen-layout';
 import ContentBox from 'components/content-box';
@@ -7,15 +9,42 @@ import AvatarEditor from 'components/avatar-editor';
 import Input from 'components/input';
 import Select from 'components/select';
 import Button from 'components/button';
+import FormGroup from 'components/form-group';
 // Styles
 import './styles.sass';
 // Assets
 import AvatarImage from 'assets/images/user/user-fullsize.jpg';
 
+const validationSchema = Yup.object().shape({
+  userName: Yup.string().required('Username is a required field'),
+  email: Yup.string().email().required('Email field is required'),
+  password: Yup.string().required('Username is a required field'),
+  timeZone: Yup.string().required('Time Zone is a required field'),
+});
+
+const timeZones = [
+  { label: 'Pacific standard', value: 'pacific standard' },
+  { label: 'Pacific standard 2', value: 'pacific standard 2' },
+  { label: 'Pacific standard 3', value: 'pacific standard 3' }
+];
+
 const SettingsScreen = () => {
-  const handleSubmit = e => {
-    e.preventDefault();
-  };
+  const formik = useFormik({
+    initialValues: {
+      userName: 'Fortbettor',
+      email: 'fortbettor@gmail.com',
+      password: '******123',
+      timeZone: 'pacific standard'
+    },
+    validationSchema,
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      setSubmitting(true);
+      setSubmitting(false);
+      resetForm();
+    },
+  });
+
+  const { handleSubmit, touched, errors, handleChange, handleBlur, values } = formik;
 
   return (
     <ScreenLayout>
@@ -25,41 +54,52 @@ const SettingsScreen = () => {
         </ContentBox.Header>
         <ContentBox.Body>
           <form className="settings-screen__body" onSubmit={handleSubmit}>
-            <div className="settings-screen__item">
-              <Typography component="h4" className="settings-screen__item-title">Profile picture</Typography>
+            <FormGroup label="Profile picture">
               <div className="settings-screen__item-box">
                 <AvatarEditor image={AvatarImage} />
               </div>
-            </div>
-            <div className="settings-screen__item">
-              <Typography component="h4" className="settings-screen__item-title">Username</Typography>
-              <div className="settings-screen__item-box">
-                <Input value="Fortbettor" onChange={() => { }} />
-              </div>
-            </div>
-            <div className="settings-screen__item">
-              <Typography component="h4" className="settings-screen__item-title">Email</Typography>
-              <div className="settings-screen__item-box">
-                <Input value="Fortbettor@gmail.com" onChange={() => { }} />
-              </div>
-            </div>
-            <div className="settings-screen__item">
-              <Typography component="h4" className="settings-screen__item-title">Password</Typography>
-              <div className="settings-screen__item-box">
-                <Input value="******123" onChange={() => { }} />
-              </div>
-            </div>
-            <div className="settings-screen__item">
-              <Typography component="h4" className="settings-screen__item-title">Time Zone</Typography>
-              <div className="settings-screen__item-box">
-                <Select
-                  defaultValue={{ label: 'Pacific standard', value: 'pacific standard' }}
-                  options={[
-                    { label: 'Pacific standard', value: 'pacific standard' },
-                    { label: 'Pacific standard', value: 'pacific standard' },
-                  ]} />
-              </div>
-            </div>
+            </FormGroup>
+            <FormGroup label="Username" errorMsg={touched.userName && errors.userName}>
+              <Input
+                type="text"
+                value={values.userName}
+                name="userName"
+                invalid={touched.email && errors.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Username"
+              />
+            </FormGroup>
+            <FormGroup label="Email" errorMsg={touched.email && errors.email}>
+              <Input
+                type="email"
+                value={values.email}
+                name="email"
+                invalid={touched.email && errors.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Email"
+              />
+            </FormGroup>
+            <FormGroup label="Password" errorMsg={touched.password && errors.password}>
+              <Input
+                type="password"
+                value={values.password}
+                name="password"
+                invalid={touched.password && errors.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Password"
+              />
+            </FormGroup>
+            <FormGroup label="Time Zone" errorMsg={touched.timeZone && errors.timeZone}>
+              <Select
+                options={timeZones}
+                value={timeZones.find(({ value }) => value === values.timeZone)}
+                onChange={({ value }) => handleChange("timeZone")(value)}
+                name="timeZone"
+              />
+            </FormGroup>
             <Button variant="primary" size="xl" type="submit">Save</Button>
           </form>
         </ContentBox.Body>
