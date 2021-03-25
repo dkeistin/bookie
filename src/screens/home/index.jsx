5840c58f39c1eb868a95875a3917f1d91223c079
dmytro <ssadowy85@gmail.com>
2021-03-25 16:36:22 +0200

    Connect Join now button to router in home screen

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 // UI
 import Container from 'components/container';
 import Typography from 'components/typography';
@@ -7,6 +8,7 @@ import Button from 'components/button';
 import './styles.sass';
 
 const HomeScreen = () => {
+  const history = useHistory();
   return (
     <div className="home-screen">
       <Container>
@@ -14,7 +16,7 @@ const HomeScreen = () => {
           <div className="home-screen__info">
             <Typography component="h1" className="home-screen__title">Join today and have your favorite players carry you to victory</Typography>
             <Typography component="p" className="p-xl home-screen__text">Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</Typography>
-            <Button variant="accent" size="xl">Join now</Button>
+            <Button variant="accent" size="xl" onClick={() => history.push('/sign-up')}>Join now</Button>
           </div>
         </div>
       </Container>
