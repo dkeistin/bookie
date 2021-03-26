a7081c5f365a7ba085dfc6d9b13d3ea89ece0f57
dmytro <ssadowy85@gmail.com>
2021-03-26 13:58:51 +0200

    Refactor overlay component
    - Add CSSTransition instead of css animations
    - Improve performance
    - Fix empty div issue

--- src/components/overlay/index.jsx
+++ src/components/overlay/index.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
 // Styles
 import './styles.sass';
 
-const Overlay = ({ isActive, className, zIndex, style, fixed, ...otherProps }) => {
+const Overlay = ({ isActive, className, zIndex, style, ...otherProps }) => {
   const [container] = React.useState(document.createElement('div'));
+  const overlayRef = React.createRef(null);
 
   React.useEffect(() => {
-    if (!fixed) {
+    if (isActive) {
       document.body.appendChild(container);
-      return () => {
-        document.body.removeChild(container);
-      }
     }
-  }, [container, fixed]);
+  }, [container, isActive]);
 
   const classes = classNames({
     'overlay': true,
-    'is-active': isActive,
     [className]: className
   });
 
-  const overlay = <div className={classes} style={{ zIndex }}  {...otherProps} />;
-
-  if (fixed) {
-    return overlay;
-  } else {
-    return ReactDOM.createPortal(overlay, container);
-  }
+  return ReactDOM.createPortal((
+    <CSSTransition nodeRef={overlayRef} in={isActive} timeout={300} onExited={() => document.body.removeChild(container)} classNames="overlay-animation" >
+      <div ref={overlayRef} className={classes} style={{ zIndex }}  {...otherProps} />
+    </CSSTransition >
+  ), container);
 };
 
 Overlay.defaultProps = {
