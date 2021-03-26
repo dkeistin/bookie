bd6d9f4e92dba27cf871d8384275834a2b608d50
dmytro <ssadowy85@gmail.com>
2021-03-26 13:39:48 +0200

    Implement deposit address copy to clipboard logic

--- src/screens/deposit/deposit-details/index.jsx
+++ src/screens/deposit/deposit-details/index.jsx
@@ -5,6 +5,7 @@ import Box from 'components/box';
 import Typography from 'components/typography';
 import Input from 'components/input';
 import Button from 'components/button';
+import Alert from 'components/alert';
 // Styles
 import './styles.sass';
 // Assets
@@ -12,12 +13,21 @@ import { ReactComponent as BitcoinIcon } from 'assets/images/coins/bitcoin.svg';
 import QrCode from 'assets/images/misc/qr.png';
 
 const DepositDetails = () => {
+  const [copySuccess, setCopySuccess] = React.useState(false);
+  const tempAddress = '3DmdyMxDew8BJPpDGW3hFUzv3jh75sN1jW';
+
+  const handleCopyClick = () => {
+    setCopySuccess(true);
+    navigator.clipboard.writeText(tempAddress);
+  };
+
   return (
     <ContentBox className="deposit-details">
       <ContentBox.Header>
         <Typography component="h3">Deposit</Typography>
       </ContentBox.Header>
       <ContentBox.Body>
+        <Alert type="success" isActive={copySuccess} onClose={() => setCopySuccess(false)}>Copied successfully</Alert>
         <div className="deposit-details__content">
           <BitcoinIcon className="deposit-details__icon" />
           <div className="deposit-details__wrap">
@@ -30,8 +40,8 @@ const DepositDetails = () => {
             </div>
             <Box className="deposit-details__item">
               <Typography component="h4" className="deposit-details__item-title">Send your bitcoin to this address</Typography>
-              <Input value="3DmdyMxDew8BJPpDGW3hFUzv3jh75sN1jW" className="deposit-details__item-input" center disabled />
-              <Button variant="primary" size="lg">Copy address</Button>
+              <Input value={tempAddress} className="deposit-details__item-input" center disabled />
+              <Button variant="primary" size="lg" onClick={handleCopyClick}>Copy address</Button>
             </Box>
           </div>
         </div>
