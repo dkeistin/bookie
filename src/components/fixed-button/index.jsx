0ea2754b2634a9754723dddce2f33d54e45ae2ff
dmytro <ssadowy85@gmail.com>
2021-03-27 10:31:42 +0200

    Add text option to fixedButton component

--- src/components/fixed-button/index.jsx
+++ src/components/fixed-button/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+// UI
+import Typography from 'components/typography';
 // Styles
 import './styles.sass';
 
-const FixedButton = ({ icon: Icon, ...otherProps }) => {
+const FixedButton = ({ icon: Icon, text, ...otherProps }) => {
   return (
     <div className="fixed-button" {...otherProps}>
+      {text.toString() && <Typography component="span" variant="p-sm" className="text-bold fixed-button__text">{text}</Typography>}
       <Icon className="fixed-button__icon" />
     </div>
   );
