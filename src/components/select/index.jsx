4e64808aa037a82801bacb73105fc1f2915e654e
dmytro <ssadowy85@gmail.com>
2021-03-29 10:47:29 +0300

    Update select mobile behaviour

--- src/components/select/index.jsx
+++ src/components/select/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TimezoneSelect, { i18nTimezones } from 'react-timezone-select';
+import { withBreakpoints } from 'react-breakpoints'
 // UI
 import ReactSelect from 'react-select';
 // Context
@@ -7,14 +8,25 @@ import { useTheme } from 'context/theme-context';
 // Styles
 import { selectStyles } from './styles';
 
-const Select = ({ options, defaultValue, timezones, ...otherProps }) => {
+const Select = ({ options, defaultValue, timezones,  breakpoints, currentBreakpoint,...otherProps }) => {
   const { dark } = useTheme();
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    if (breakpoints[currentBreakpoint] < breakpoints.md) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [breakpoints, currentBreakpoint]);
 
   if (timezones) {
     return (
       <TimezoneSelect
         styles={selectStyles(dark)}
         timezones={i18nTimezones}
+        menuShouldScrollIntoView={isMobile}
+        menuShouldBlockScroll={isMobile}
         {...otherProps} />
     )
   }
@@ -24,9 +36,11 @@ const Select = ({ options, defaultValue, timezones, ...otherProps }) => {
       styles={selectStyles(dark)}
       options={options}
       defaultValue={defaultValue}
+      menuShouldScrollIntoView={isMobile}
+      menuShouldBlockScroll={isMobile}
       {...otherProps}
     />
   )
 };
 
-export default Select;
+export default withBreakpoints(Select);
