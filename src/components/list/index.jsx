5f5ab9697403c1661b3809330f3eafa61ca7f224
dmytro <ssadowy85@gmail.com>
2021-03-29 13:57:27 +0300

    Improve accessability for list module

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import classNames from 'classnames';
 // UI
 import Typography from 'components/typography';
+// Helpers
+import { handleAccessibilityKeyPress } from 'utils/helpers';
 // Styles
 import './styles.sass';
 
@@ -12,14 +14,16 @@ const List = ({ header, items, handleSelect, alt, blocked, selected }) => {
     'list--blocked': blocked,
   });
 
+  const handleItem = id => () => handleSelect(id);
   const isActive = currentId => selected.some(({ id }) => id === currentId);
+  const handleKeyPress = id => e => handleAccessibilityKeyPress(e, handleItem(id));
 
   return (
     <div className={classnames}>
       <Typography component="h6" className="list__header">{header}</Typography>
       <div className="list__items">
         {items.map(({ id, game, price }) => (
-          <div key={id} className={`list__item ${isActive(id) ? 'is-active' : ''}`} onClick={() => { handleSelect(id) }}>
+          <div key={id} className={`list__item ${isActive(id) ? 'is-active' : ''}`} onClick={handleItem(id)} onKeyPress={handleKeyPress(id)} tabIndex="0">
             <Typography component="span" variant="p-sm" className="text-medium list__item-game">{game}</Typography>
             <Typography component="span" variant="h6">{price}</Typography>
           </div>
