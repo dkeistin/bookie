7bdcafab53cf7ec705a6f7088de5df7dc87384c2
dmytro <ssadowy85@gmail.com>
2021-03-31 21:20:23 +0300

    Add otherProps to scrollbar component

--- src/components/scrollbar/index.jsx
+++ src/components/scrollbar/index.jsx
@@ -4,14 +4,14 @@ import classNames from 'classnames';
 // Styles
 import './styles.sass';
 
-const Scrollbar = ({ children, className, maxHeight }) => {
+const Scrollbar = ({ children, className, maxHeight, ...otherProps }) => {
   const classes = classNames({
     'custom-scroll': true,
     [className]: className
   });
 
   return (
-    <SimpleBar className={classes} style={{ maxHeight }}>
+    <SimpleBar className={classes} style={{ maxHeight }} {...otherProps}>
       {children}
     </SimpleBar>
   );
