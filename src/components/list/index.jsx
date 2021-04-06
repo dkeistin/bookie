03902a2ba4b8e2a729562586c8aabdee75d2464b
dmytro <ssadowy85@gmail.com>
2021-04-06 10:37:59 +0300

    Fix list-item click focus issue

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -14,16 +14,19 @@ const List = ({ header, items, handleSelect, alt, blocked, selected }) => {
     'list--blocked': blocked,
   });
 
-  const handleItem = id => () => handleSelect(id);
   const isActive = currentId => selected.some(({ id }) => id === currentId);
-  const handleKeyPress = id => e => handleAccessibilityKeyPress(e, handleItem(id));
+  const handleClick = id => e => {
+    handleSelect(id);
+    e.currentTarget.blur();
+  };
+  const handleKeyPress = id => e => handleAccessibilityKeyPress(e, handleSelect.bind(null, id));
 
   return (
     <div className={classnames}>
       <Typography component="h6" className="list__header">{header}</Typography>
       <div className="list__items">
         {items.map(({ id, game, price }) => (
-          <div key={id} className={`list__item ${isActive(id) ? 'is-active' : ''}`} onClick={handleItem(id)} onKeyPress={handleKeyPress(id)} tabIndex="0">
+          <div key={id} className={`list__item ${isActive(id) ? 'is-active' : ''}`} onClick={handleClick(id)} onKeyPress={handleKeyPress(id)} tabIndex="0">
             <Typography component="span" variant="p-sm" className="text-medium list__item-game">{game}</Typography>
             <Typography component="span" variant="h6">{price}</Typography>
           </div>
