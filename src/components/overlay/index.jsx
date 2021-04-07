13c08e99d0cbcbe29588a223706f5d1983866f28
dmytro <ssadowy85@gmail.com>
2021-04-07 16:59:19 +0300

    Refactor overlay component

--- src/components/overlay/index.jsx
+++ src/components/overlay/index.jsx
@@ -6,12 +6,12 @@ import { CSSTransition } from 'react-transition-group';
 // Styles
 import './styles.sass';
 
-const Overlay = ({ isActive, className, zIndex, ...otherProps }) => {
+const Overlay = ({ isActive, className, zIndex, inlined, ...otherProps }) => {
   const [container] = React.useState(document.createElement('div'));
   const overlayRef = React.createRef(null);
 
   React.useEffect(() => {
-    if (isActive) {
+    if (!inlined && isActive) {
       document.body.appendChild(container);
     }
   }, [container, isActive]);
@@ -21,11 +21,17 @@ const Overlay = ({ isActive, className, zIndex, ...otherProps }) => {
     [className]: className
   });
 
-  return ReactDOM.createPortal((
-    <CSSTransition nodeRef={overlayRef} in={isActive} timeout={300} onExited={() => document.body.removeChild(container)} classNames="overlay-animation" >
+  const overlay = (
+    <CSSTransition nodeRef={overlayRef} in={isActive} timeout={300} onExited={() => !inlined && document.body.removeChild(container)} classNames="overlay-animation" unmountOnExit={inlined}>
       <div ref={overlayRef} className={classes} style={{ zIndex }}  {...otherProps} />
-    </CSSTransition >
-  ), container);
+    </CSSTransition>
+  );
+
+  if (inlined) {
+    return overlay;
+  }
+
+  return ReactDOM.createPortal(overlay, container);
 };
 
 Overlay.defaultProps = {
@@ -34,6 +40,7 @@ Overlay.defaultProps = {
 
 Overlay.propTypes = {
   isActive: PropTypes.bool,
+  inlined: PropTypes.bool,
   className: PropTypes.string,
   children: PropTypes.node,
   zIndex: PropTypes.oneOfType([
