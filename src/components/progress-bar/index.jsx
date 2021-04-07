f5be72946e593618fdf786e26e9a5ca4a1b45c1c
dmytro <ssadowy85@gmail.com>
2021-04-07 15:35:06 +0300

    Refactor progress-bar component according to the eslinter styles

--- src/components/progress-bar/index.jsx
+++ src/components/progress-bar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 // Styles
 import './styles.sass';
@@ -13,6 +14,11 @@ const ProgressBar = ({ fill, className }) => {
       <div className="progress-bar__line" style={{ width: `${fill}%` }}></div>
     </div>
   );
+};
+
+ProgressBar.propTypes = {
+  fill: PropTypes.number,
+  className: PropTypes.string,
 };
 
 export default ProgressBar;
