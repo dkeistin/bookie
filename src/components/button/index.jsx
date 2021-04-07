2435e81ed12a6af1d332c38dccd63ed70b3489e2
dmytro <ssadowy85@gmail.com>
2021-04-07 14:32:37 +0300

    Refactor button component according to the eslinter styles

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 // Styles
 import './styles.sass';
@@ -27,5 +28,19 @@ const Button = ({ children, href, className, variant, size, disabled, onClick, f
   );
 };
 
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  href: PropTypes.string,
+  className: PropTypes.string,
+  variant: PropTypes.string,
+  size: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  fluid: PropTypes.bool,
+  noShadow: PropTypes.bool,
+  type: PropTypes.string,
+  style: PropTypes.object,
+  loading: PropTypes.bool,
+};
 
 export default Button;
