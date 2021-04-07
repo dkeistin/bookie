0b623c2ebdf2aa2791c08898b746149d15c05e32
dmytro <ssadowy85@gmail.com>
2021-04-07 14:30:42 +0300

    Refactor box component according to the eslinter styles

--- src/components/box/index.jsx
+++ src/components/box/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 // Styles
 import './styles.sass';
@@ -14,6 +15,11 @@ const Box = ({ children, className, ...otherProps }) => {
       {children}
     </div>
   );
+};
+
+Box.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default Box;
