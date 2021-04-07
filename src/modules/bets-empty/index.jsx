86af84282b25c0c0c783ee0795993d1aa25a2bbe
dmytro <ssadowy85@gmail.com>
2021-04-07 16:07:50 +0300

    Refactor bets-empty module according to the eslinter styles

--- src/modules/bets-empty/index.jsx
+++ src/modules/bets-empty/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // UI
 import Typography from 'components/typography';
 // Styles
@@ -13,6 +14,10 @@ const BetsEmpty = ({ title }) => {
       </Typography>
     </div>
   );
+};
+
+BetsEmpty.propTypes = {
+  title: PropTypes.string,
 };
 
 export default BetsEmpty;
