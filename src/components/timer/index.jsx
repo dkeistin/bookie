7375e10396730b204ce94097cc49fc61945a2d0a
dmytro <ssadowy85@gmail.com>
2021-04-07 15:50:01 +0300

    Refactor timer component according to the eslinter styles

--- src/components/timer/index.jsx
+++ src/components/timer/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 // Styles
 import './styles.sass';
@@ -14,6 +15,11 @@ const Timer = ({ time, className }) => {
       {time}
     </div>
   );
+};
+
+Timer.propTypes = {
+  time: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default Timer;
